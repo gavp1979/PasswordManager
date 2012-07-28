5ebcb421cd45ecd705d94ae941d2e5d4b850520b
Gav Parkes <gav@parkesfamily.co.uk>
2012-07-28 16:55:17 +0100

    Added DB Classes

--- passwordmanager/Password Manager/src/com/example/password/manager/MainActivity.java
+++ passwordmanager/Password Manager/src/com/example/password/manager/MainActivity.java
@@ -1,11 +1,22 @@
 package com.example.password.manager;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
+	private static final String MASTER_PASSWORD	= "StupidFuckingPasswords";
+	
+	private boolean _bUnlocked	= false;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,4 +29,51 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) 
+	{
+		boolean bHandled = true;
+		
+		switch (item.getItemId()) 
+		{
+			case R.id.mniAdd:
+				Intent intDetails = new Intent(this, PasswordDetails.class);
+				startActivity(intDetails);
+				break;
+	
+			default:
+				bHandled = super.onOptionsItemSelected(item); 
+				break;
+		}
+		
+		return bHandled;
+	}
+	
+	public void btnClick(View view)
+	{
+		if (view.getId() == R.id.btnUnlock)
+		{
+			final EditText edtPassword = new EditText(this);
+			
+			AlertDialog.Builder builder = new AlertDialog.Builder(this);
+			builder.setTitle("Unlock");
+			builder.setMessage("To unlock the application please enter the master password below");
+			builder.setView(edtPassword);
+			builder.setPositiveButton("Unlock", new OnClickListener() {
+				
+				@Override
+				public void onClick(DialogInterface dialog, int which) 
+				{
+					final String strPassword = edtPassword.getText().toString().trim();
+					
+					if (strPassword.equalsIgnoreCase(MASTER_PASSWORD))
+						_bUnlocked = true;
+				}
+			});
+			builder.show();
+		}
+	}
+    
+    
 }
