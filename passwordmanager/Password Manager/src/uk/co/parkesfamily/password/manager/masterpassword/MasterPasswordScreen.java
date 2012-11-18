05577e62bd79d7be45475f3abd07c36b1e52a169
Administrator <Administrator@stafflaptop9.aldwyn.local>
2012-11-18 18:35:16 +0000

    a

--- passwordmanager/Password Manager/src/uk/co/parkesfamily/password/manager/masterpassword/MasterPasswordScreen.java
+++ passwordmanager/Password Manager/src/uk/co/parkesfamily/password/manager/masterpassword/MasterPasswordScreen.java
@@ -1,10 +1,16 @@
 package uk.co.parkesfamily.password.manager.masterpassword;
+import uk.co.parkesfamily.password.manager.R;
+import uk.co.parkesfamily.password.manager.helperclasses.DBHelper;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-
-import uk.co.parkesfamily.password.manager.R;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * @author Administrator
@@ -12,6 +18,8 @@ import uk.co.parkesfamily.password.manager.R;
  */
 public class MasterPasswordScreen extends FragmentActivity
 {
+	EditText _edtOldPassword, _edtNewPassword, _edtConfirmPassword;
+	
 	public static Intent createIntent(Context context)
 	{
 		Intent intResult = new Intent(context, MasterPasswordScreen.class);
@@ -24,6 +32,38 @@ public class MasterPasswordScreen extends FragmentActivity
 	{
 		super.onCreate(arg0);
 		setContentView(R.layout.master_password_screen);
+		
+		_edtNewPassword = (EditText) findViewById(R.id.edtExistingPassword);
+		_edtOldPassword = (EditText) findViewById(R.id.edtNewPassword);
+		_edtConfirmPassword = (EditText) findViewById(R.id.edtRetypePassword);
 	}
 
+	public void btnOk_Click(View view)
+	{
+		Toast.makeText(this, "OK button clicked", Toast.LENGTH_LONG).show();
+		
+		final String strOldPassword = DBHelper.getMasterPassword(getContentResolver());
+		final String strNewPassword = 
+				
+		if 
+	}
+	
+	public void btnCancel_Click(View view)
+	{
+		//Toast.makeText(this, "Cancel button clicked", Toast.LENGTH_LONG).show();
+		AlertDialog.Builder dlg = new AlertDialog.Builder(this);
+		dlg.setTitle("Confirmation");
+		dlg.setMessage("Are you sure you want to leave without saving?");
+		dlg.setPositiveButton("Yes", new OnClickListener()
+		{
+			
+			@Override
+			public void onClick(DialogInterface dialog, int which)
+			{
+				finish();
+			}
+		});
+		dlg.setNegativeButton("No", null);
+		dlg.show();
+	}
 }
